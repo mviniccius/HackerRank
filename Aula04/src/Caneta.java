2b3d9ab333c664b61b5c4ac570343fd5d971f2dd
mviniccius <marcus.viniccius@hotmail.com>
2023-10-06 18:33:56 -0300

    Aula do curso em video do Guanabara

---
+++ Aula04/src/Caneta.java
@@ -0,0 +1,27 @@
+public class Caneta {
+
+    public String modelo;
+    private float ponta;
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+
+    public void status(){
+        System.out.println("Dados da caneta!");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: "+ this.ponta);
+    }
+
+}
